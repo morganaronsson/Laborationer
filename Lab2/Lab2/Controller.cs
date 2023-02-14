e6744c4843913f3cbccf66b98426dfbcea6b0fff
morganaronsson <morgan_1998@hotmail.se>
2023-02-14 16:44:05 +0100

    First real commit

---
+++ Lab2/Lab2/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2
+{
+    internal class Controller
+    {
+
+        public void AddEntry(Person p, string phonenumber)
+        {
+            HashSet<Phone> phones = FindPhones(p);
+
+            foreach(Phone phone in phones)
+            {
+                if(!phone.PhoneNumber.Equals(phonenumber))
+                {
+                    Phone newPhone = new Phone(phonenumber);
+                    phones.Add(newPhone);
+                }
+            }
+        }
+
+        public HashSet<Phone> FindPhones (Person p)
+        {        
+                return p.phones;
+        }
+
+        public void RemoveEntry(Person p, string oldnumber)
+        {
+            HashSet<Phone> phones = FindPhones(p);
+
+            foreach(Phone phone in phones) 
+            {
+                if(phone.PhoneNumber.Equals(oldnumber))
+                {
+                    phones.Remove(phone);
+                }
+            }
+        }
+
+    }
+}
--- Lab2/Lab2/PhoneDB.cs
+++ Lab2/Lab2/PhoneDB.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lab2
 {
-    internal class PhoneDB
+    abstract class PhoneDB
     {
+        private HashSet<string> members;
+        private HashSet<string> telephones = new HashSet<string>();
+
+        public HashSet<string> FindPhones(string name)
+        {
+            HashSet<string> phonesforaperson = new HashSet<string>();
+            foreach (string s in telephones)
+            {
+                if (telephones.Contains(s))
+                {
+                    phonesforaperson.Add(s);
+                }
+            }
+            return phonesforaperson;
+        }
+
+        public void AddEntry(string name, string phonenumber)
+        {
+            HashSet<string> phones = FindPhones(name);
+
+            foreach (string phone in phones)
+            {
+                if (!phone.Contains(phonenumber))
+                {
+                    telephones.Add(name + " " + phonenumber);
+                    
+                }
+            }
+        }
+
+        public void RemoveEntry(string name, string oldnumber)
+        {
+            HashSet<string> phones = FindPhones(name);
+
+            foreach (string phone in phones)
+            {
+                if (phone.Contains(oldnumber))
+                {
+                    if (phone.Contains(name))
+                    {
+                        telephones.Remove(phone);
+                    }
+                }
+            }
+        }
     }
 }
